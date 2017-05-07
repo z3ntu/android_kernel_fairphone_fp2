dd6e0331f850ae517f63d3f760813a213872299f
Luca Weiss <luca@z3ntu.xyz>
2017-05-07 18:04:36 +0200

    Better make.sh script

--- make.sh
+++ make.sh
@@ -5,6 +5,19 @@
 # 2. Run ./make.sh <nr-of-cpu-cores>
 # 3. Enjoy your arch/arm/boot/zImage!
 
+prebuiltloc=/home/luca/android/prebuilt
+
+# Linaro toolchain
+# Get the toolchain from http://releases.linaro.org/archive/14.06/components/toolchain/binaries/gcc-linaro-arm-linux-gnueabihf-4.9-2014.06_linux.tar.xz
+# Note: It probably won't work!
+#toolchain=gcc-linaro-arm-linux-gnueabihf-4.9-2014.06_linux
+#crossprefix=arm-linux-gnueabihf-
+
+# AOSP toolchain
+# Clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 into $prebuildloc
+toolchain=arm-linux-androideabi-4.9
+crossprefix=arm-linux-androideabi-
+
 # Define the help function
 function help() {
 cat <<EOF
@@ -26,23 +39,28 @@ export ARCH=arm
 export SUBARCH=arm
 
 # "ln -s /usr/bin/python2 ./python" in that folder.
-export PATH=/home/luca/android/prebuilt/compat:$PATH
-# Get the toolchain from http://releases.linaro.org/archive/14.06/components/toolchain/binaries/gcc-linaro-arm-linux-gnueabihf-4.9-2014.06_linux.tar.xz
-# Note that gcc 4.9.1 is confirmed to work but 4.9.4 is throwing kernel section mismatches.
-export CROSS_COMPILE=/home/luca/android/prebuilt/gcc-linaro-arm-linux-gnueabihf-4.9-2014.06_linux/bin/arm-linux-gnueabihf-
+export PATH=$prebuiltloc/compat:$PATH
+
+export CROSS_COMPILE=$prebuiltloc/$toolchain/bin/$crossprefix
 
 # Make sure build is clean!
 echo 'Cleaning up.'
 make clean
 
 # Generates a new .config and exits
-if [ "$1" = "config" ] ; then
+if [ "$1" = "config" ]; then
     config=$2
     if [ ! $config ]; then
         config=lineageos_fp2_defconfig
     fi
     echo "Running 'make $config'"
     make "$config"
+    exit
+fi
+
+if [ "$1" = "menuconfig" ]; then
+    echo "Running 'make menuconfig'"
+    make menuconfig
     exit
 fi
 
@@ -54,4 +72,4 @@ fi
 
 # Compile
 echo 'Start the compilation.'
-make -j$1
+make -j$1 CONFIG_DEBUG_SECTION_MISMATCH=y
